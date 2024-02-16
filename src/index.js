16aef462121f70b0fdf7dc717bf2a14769268f7d
mariaatalig <156150657+ataligmaria@users.noreply.github.com>
2024-02-16 12:31:58 -0500

    Added Axios and search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeatherTemp(response) {
+  let currentTemp = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  currentTemp.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "e36ba1fe4acob8a700a38tc4c3b503db";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(refreshWeatherTemp);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Miami");
