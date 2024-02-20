71ab2ba7cba3897418318edfb3ec1b9894238b19
mariaatalig <156150657+ataligmaria@users.noreply.github.com>
2024-02-19 19:41:35 -0500

    Utilized the API to display the forecast

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeatherTemp(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   currentTemp.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class = "weather-app-icon" />`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -53,5 +55,49 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "e36ba1fe4acob8a700a38tc4c3b503db";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="row">
+          <div class="col-2">
+            <div class="weather-forecast-day">${formatDay(day.time)}</div>
+            <img src="${
+              day.condition.icon_url
+            }" class="weather-forecast-icon" />
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.minimum)}&deg;</strong>
+              </span>
+              <span class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}&deg;</span>
+            </div>
+          </div>
+        </div>
+        `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 searchCity("Miami");
